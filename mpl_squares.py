7589cb93e8bdd814a88139978aaca36a2d893720
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-07-02 23:57:15 +0800

    scatter plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+# ax.plot(input_values, squares, linewidth=3)
+
+ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
